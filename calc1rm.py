5835d183c537512adced791919c1cadcc0a3adb9
Obi Baratt <obibaratt@gmail.com>
2022-05-11 15:53:43 -0700

    Added calc1rm.py with class to handle 1rm calculations, squat_overload workout

---
+++ calc1rm.py
@@ -0,0 +1,19 @@
+class MaxCalc:
+    def __init__(self):
+        self.max_reps = {1: 1, 2: 0.97, 3: 0.94, 4: 0.92, 5: 0.89,
+                         6: 0.86, 7: 0.83, 8: 0.81, 9: 0.78, 10: 0.75,
+                         11: 0.73, 12: 0.71, 13: 0.70, 14: 0.68, 15: 0.67,
+                         16: 0.65, 17: 0.64, 18: 0.63, 19: 0.61, 20: 0.60}
+
+        self.one_rm = 0
+
+    def calc_one_rm(self, reps, weight):
+        max_percent = self.max_reps[reps]
+        self.one_rm = int(weight / max_percent)
+        return self.one_rm
+
+
+calc = MaxCalc()
+
+my_max = calc.calc_one_rm(5, 365)
+print(my_max)
---
+++ squat_overload.py
@@ -0,0 +1,39 @@
+from calc1rm import MaxCalc
+
+calc = MaxCalc()
+
+max_dict_from_calc1rm = calc.max_reps
+
+
+class SquatWorkout:
+    def __init__(self, one_rm, max_dict):
+        self.one_rm = one_rm
+        self.max_dict = max_dict
+
+        self.rep_hi = int(self.max_dict[3] * self.one_rm)
+        self.rep_mid = int(self.max_dict[9] * self.one_rm)
+        self.rep_low = int(self.max_dict[12] * self.one_rm)
+
+        self.rep_4 = int(self.max_dict[4] * self.one_rm)
+        self.rep_6 = int(self.max_dict[6] * self.one_rm)
+        self.rep_20 = int(self.max_dict[20] * self.one_rm)
+
+    def day1(self):
+        return f'{self.rep_hi}x3, 2, 1\n{self.rep_mid}x8\n{self.rep_low}x10'
+
+    def day2(self):
+        return f'{self.rep_hi}x5, 3, 1\n{self.rep_mid}x8\n{self.rep_low}x8'
+
+    def day3(self):
+        return f'{self.rep_mid}x5\n{self.rep_6}x3\n{self.rep_4}x1\n{self.rep_hi}x1\n{self.rep_20}x20'
+
+
+squat = SquatWorkout(410, max_dict_from_calc1rm)
+
+print(squat.day1())
+print()
+print(squat.day2())
+print()
+print(squat.day3())
+
+
