a3873a40c29fb2065cc2bd0eeeec30c2f20436a5
Obi Baratt <obibaratt@gmail.com>
2022-06-03 14:44:22 -0700

    Removed forms from app.py and made into module. Added login / register forms.

---
+++ forms.py
@@ -0,0 +1,42 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, SubmitField, SelectField, StringField, PasswordField
+from wtforms.validators import DataRequired, Optional
+
+
+class InputForm(FlaskForm):
+    weight = IntegerField(label='Weight', validators=[DataRequired()])
+    reps = IntegerField(label='Reps', default=1, validators=[DataRequired()])
+    # TODO: Add alternate workouts
+    workout = SelectField(label='Workout', choices=[('overload', 'Squat Overload'),
+                                                    ('nuckolsSquat', 'Nuckols Squat'),
+                                                    ('nuckolsPress', 'Nuckols Press'),
+                                                    ('nuckolsDeadlift', 'Nuckols Deadlift')])
+    submit = SubmitField(label='Submit')
+
+
+class MaxForm(FlaskForm):
+    new_max = IntegerField(label='New Max', validators=[DataRequired()])
+    exercise = SelectField(label='Exercise', choices=[('squat', 'Squat'),
+                                                      ('deadlift', 'Deadlift'),
+                                                      ('bench', 'Bench'),
+                                                      ('overhead', 'Overhead')])
+    submit = SubmitField(label='Update Maxes (Blank = no change)')
+
+
+class OneRmForm(FlaskForm):
+    weight = IntegerField(label='Weight', validators=[DataRequired()])
+    reps = IntegerField(label='Reps', validators=[DataRequired()])
+    submit = SubmitField(label='Calculate')
+
+
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("Register")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
